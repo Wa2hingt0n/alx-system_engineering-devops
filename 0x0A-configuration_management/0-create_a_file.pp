b37ca39c3f7c325f0f4b11987ab4e7057570d830
Washington Obungu <tobungu@gmail.com>
2022-03-11 04:45:14 -0800

    Created a file using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file in the /tmp directory
+
+file { '/tmp/school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
